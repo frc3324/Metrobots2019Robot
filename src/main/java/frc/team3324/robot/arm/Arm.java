49ec5e7c7a702de634951b4ee029ed984ac5ca33
Mani Kamali <maniyar@pop-os.localdomain>
2019-02-04 16:22:29 -0500

    Wrote a simple Victor wrapper (#24)
    Victor wrapper implements everything we use from the Victor, we
    can switch motor controllers and only change this file.

--- src/main/java/frc/team3324/robot/arm/Arm.java
+++ src/main/java/frc/team3324/robot/arm/Arm.java
@@ -1,5 +1,6 @@
 package frc.team3324.robot.arm;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import frc.team3324.robot.arm.commands.ControlArm;
 import frc.team3324.robot.util.Constants;
 
@@ -10,19 +11,16 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Arm extends Subsystem {
     // TODO Invert stuff
-    private WPI_VictorSPX armMotorLeft     = new WPI_VictorSPX(Constants.Arm.MOTOR_PORT_ARM_LEFT);
+    private WPI_TalonSRX armMotorLeft     = new WPI_TalonSRX(Constants.Arm.MOTOR_PORT_ARM_LEFT);
     private WPI_VictorSPX armMotorRight    = new WPI_VictorSPX(Constants.Arm.MOTOR_PORT_ARM_RIGHT);
     private SpeedControllerGroup armMotors = new SpeedControllerGroup(armMotorLeft, armMotorRight);
 
     public Arm() {
+        armMotorRight.follow(armMotorLeft);
         // CAUTION: direction already set, don't change it
         armMotorLeft.setInverted(true);
     }
 
-    /**
-     * Move the arm at the specified speed.
-     * @param speed
-     */
     public void setArmSpeed(double speed) { armMotors.set(speed); }
 
     public void initDefaultCommand() { setDefaultCommand(new ControlArm()); }
